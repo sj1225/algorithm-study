db519138becae932b2d3e4e119be9b69ca7f7366
sj1225 <49314605+sj1225@users.noreply.github.com>
2024-06-18 20:45:03 +0900

    [level 1] Title: 약수의 합, Time: 0.09 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,23 +1,12 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        List<Integer> yaksu = new ArrayList<>();
-        
-        if(n == 0){
-            return 0;
-        }
-        
         for(int i=1;i<=n;i++){
             if(n % i == 0){
-                yaksu.add(i);
-                //yaksu.add(n/i);
+                answer += i;
             }
         }
-        
-        answer = yaksu.stream().mapToInt(Integer::intValue).sum();
         
         return answer;
     }

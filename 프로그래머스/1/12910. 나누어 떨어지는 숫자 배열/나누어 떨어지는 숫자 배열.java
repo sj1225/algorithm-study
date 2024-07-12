8c0649cef5ea0143161599c564c34700190aab0a
sj1225 <49314605+sj1225@users.noreply.github.com>
2024-07-12 21:41:58 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 2.80 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        int[] result = Arrays.stream(arr)
+                             .filter(a -> a % divisor == 0)
+                             .sorted()
+                             .toArray();
+        
+        return result.length == 0 ? new int[]{-1} : result;
+    }
+}

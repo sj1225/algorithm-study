ee460dc6919617e104e195d2e7ee7168d4361fbd
sj1225 <49314605+sj1225@users.noreply.github.com>
2024-07-12 22:03:10 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 1.08 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,7 +1,6 @@
 class Solution {
     public String solution(String phone_number) {
         int num = phone_number.length() - 4;
-        
         return "*".repeat(num) + phone_number.substring(num);
     }
 }

31394cdd7d593593b321d42215c3c1cb90a69ed5
sj1225 <49314605+sj1225@users.noreply.github.com>
2024-08-23 23:58:39 +0900

    [level 2] Title: 피보나치 수, Time: 2.95 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    long[] arr = new long[100001];
+    
+    public int solution(int n) {
+        arr[0] = 0; arr[1] = 1;
+        
+        for(int i=2;i<=n;i++){
+            arr[i] = (arr[i-1] + arr[i-2]) % 1234567;
+        }
+        
+        return (int)arr[n];
+    }
+}
